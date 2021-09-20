bf3edec92351656be295de9cadb47f8ea13cae47
mnikrsa21 <admin@mnikrsa.my.id>
2021-09-20 12:12:53 +0700

    Initial commit

---
+++ install.sh
@@ -0,0 +1,7 @@
+pkg install nodejs -y
+pkg install ffmpeg -y
+pkg install imagemagick -y
+npm install
+npm update
+
+echo "jika saat jalankan main.js ada bacaan module not found, tinggal npm i (yang not found)"
